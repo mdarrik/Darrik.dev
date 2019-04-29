698cfa218ffa9e13a4db090f389c52a092e66ac3
Darrk Moberg <30670444+mdarrik@users.noreply.github.com>
2019-04-28 20:29:16 -0700

    Customizes config to be more relevant to site

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Darrik Moberg`,
+    description: `Darrik is a full-stack developer with a strong interest in the front end.`,
+    author: `Darrik Moberg`,
+    keywords: [`Web Development`, `HTMl`, `CSS`, `JavaScript`, `JS`]
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -18,17 +19,17 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `DarrikMoberg`,
+        short_name: `DMoberg`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#4183d7;`,
+        theme_color: `#4183d7;`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
   ],
 }
