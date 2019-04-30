f39aef8377f6096e38c9efb64de052fa17f6a6b9
Darrk Moberg <30670444+mdarrik@users.noreply.github.com>
2019-04-30 12:08:59 -0700

    Adds author to query.
    This allows author to be updated in siteMetadata instead of manual updates.
    
    Deconstructs query instead of using inline-access for readability.

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -6,43 +6,47 @@ import Header from "./header"
 import "./layout.css"
 
 export default function Layout({ children }) {
-
   const query = useStaticQuery(
     graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
+      query SiteTitleQuery {
+        site {
+          siteMetadata {
+            title
+            author
+          }
         }
       }
-    }
-    
     `
   )
 
-  return  (
+  const {
+    site: {
+      siteMetadata: { author, title },
+    },
+  } = query
+
+  return (
     <>
-      <Header siteTitle={query.site.siteMetadata.title} />
-      <div 
+      <Header siteTitle={title} />
+      <div
         style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `0px 1.0875rem 1.45rem`,
-        paddingTop: 0,
+          margin: `0 auto`,
+          maxWidth: 960,
+          padding: `0px 1.0875rem 1.45rem`,
+          paddingTop: 0,
         }}
       >
         <main>{children}</main>
         <footer>
-
-          &copy; 
-          {new Date().getFullYear()}
-          , Built With
-          {' '}
+          &copy;
+          {author}
+          {" "}
+          {new Date().getFullYear()}, Built With{" "}
           <a href="https://www.gatsbyjs.org">Gatsby</a>
         </footer>
       </div>
     </>
-)
+  )
 }
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
