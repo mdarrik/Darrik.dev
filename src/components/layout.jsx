3e2c0a6bb80a6acd3decd5c1851c45324eef3a4c
Darrk Moberg <30670444+mdarrik@users.noreply.github.com>
2019-05-03 21:11:33 -0700

    Refactors layout. Removes inline styles and body wrapper.

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -28,23 +28,11 @@ export default function Layout({ children }) {
   return (
     <>
       <Header siteTitle={title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
-          paddingTop: 0,
-        }}
-      >
-        <main>{children}</main>
+      <main>{children}</main>
         <footer>
           &copy;
-          {author}
-          {" "}
-          {new Date().getFullYear()}, Built With{" "}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
+        {author} {new Date().getFullYear()}
         </footer>
-      </div>
     </>
   )
 }
