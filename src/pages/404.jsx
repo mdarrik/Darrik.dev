35a3412f9e62c6a672bf11da6f3495741265d2f2
Darrk Moberg <30670444+mdarrik@users.noreply.github.com>
2019-05-04 11:40:48 -0700

    Removes keywords from 404 page and  alters text.

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -5,9 +5,9 @@ import SEO from "../components/seo"
 
 const NotFoundPage = () => (
   <Layout>
-    <SEO title="404: Not found" />
+    <SEO title="404: Page Not found" keywords={['']} />
     <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <p>The page you requested doesn&#39;t exist.</p>
   </Layout>
 )
 
