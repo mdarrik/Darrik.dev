ead76de053aa23177230dd6ca4d28135d9805ec7
Darrk Moberg <30670444+mdarrik@users.noreply.github.com>
2019-07-18 20:53:28 -0700

    refactor: updates 404 to use tailwind.
    Adds a 404 image to fill space.

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,14 +1,23 @@
 import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Page Not found" keywords={['']} />
-    <h1>NOT FOUND</h1>
-    <p>The page you requested doesn&#39;t exist.</p>
-  </Layout>
-)
+const NotFoundPage = () => {
+  return (
+    <Layout>
+      <SEO title="404: Page Not found" keywords={[""]} />
+      <h1 className="text-2xl font-semibold my-4">NOT FOUND</h1>
+      <p>The page you requested doesn&#39;t exist.</p>
+      <img
+        src={require("../images/undraw_page_not_found.svg")}
+        alt=""
+        className="w-full md:w-4/5"
+      />
+    </Layout>
+  )
+}
 
 export default NotFoundPage
