00b566421ccd51cf0cc7da6eed2bda2d07eaf682
Darrk Moberg <30670444+mdarrik@users.noreply.github.com>
2019-07-20 21:36:55 -0700

    refactor: redesigns the index page with tailwind

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,35 +3,45 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-import "./index.css"
-
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <div className="w-80ch">
-      <h1>I&apos;m Darrik Moberg</h1>
-      <div className="d-flex justify-center">
-        <section className="text-card">
-          <h2>About Me</h2>
-          I&apos;m a full stack developer with a life sciences background
-          currently working for the State of Washington. I enjoy working on
-          user-centric products and using technology to streamline experiences
-          and workflows.
-        </section>
+    <hgroup className="mb-4 flex flex-col justify-center mx-auto md:block">
+      <h1 className="font-semibold md:text-3xl text-2xl mb-1 font-decorative md:inline">
+        I'm Darrik Moberg &mdash;{" "}
+      </h1>
+      <h2 className="md:text-2xl text-xl font-sans md:inline">
+        Full Stack Developer
+      </h2>
+    </hgroup>
+    <div className="d-flex justify-center">
+      <div className="shadow-lg mb-8 border-t-4 border-green-300 md:mx-8 md:px-8 px-2 pb-4 pt-2">
+        <p className="ml-2">
+          I have a life sciences background and am currently working in the
+          public sector. I mostly do web &amp; backend stuff. I enjoy working on
+          user-centric products and using technology to streamline user
+          experiences and workflows.
+        </p>
       </div>
-      <section>
-        <h2>Things I've Made</h2>
-        <ul className="project-list">
-          <li>
-            <h3>
-              <a href="https://sharktank-collaboration-presentation.netlify.com">
-                L&I IT Shark Tank Presentation{" "}
-                <span className="status">(Completed)</span>
-              </a>
-            </h3>
-            <div>
-              My work started a version of Shark Tank where employees could
-              pitch ideas directly to IT management. I pitched an idea to
+    </div>
+    <section className="mb-4">
+      <h2 className="mb-4 text-xl font-semibold font-decorative">
+        Things I've Made
+      </h2>
+      <ul className="md:px-8">
+        <li className="mb-4 shadow-lg border-t-4 border-red-600 px-4">
+          <h3 className="text-lg mb-2">
+            <a href="https://sharktank-collaboration-presentation.netlify.com">
+              IT Shark Tank Presentation{" "}
+              <span className="text-sm text-green-700 flex justify-center md:inline">
+                (Completed)
+              </span>
+            </a>
+          </h3>
+          <p className="flex flex-col">
+            <span>
+              My employer started a version of "Shark Tank" where employees
+              could pitch ideas directly to IT management. I pitched an idea to
               improve collaboration and communication in the division by using a
               forum to centralize knowledge. Two other people submitted similar
               ideas, so they grouped us together. To help make our presentation
@@ -41,39 +51,43 @@ const IndexPage = () => (
               <a href="https://www.netlify.com/">Netlify</a>. This site served
               as the slideshow for our presentation and was a big hit among both
               the "Sharks" and the 2 peers I presented with.
-              <a
-                href="https://github.com/mdarrik/sharktank-collaboration-presentation"
-                className="code-link"
-              >
-                See Code
-              </a>
-            </div>
-          </li>
-          <li>
-            <h3>
-              <a href="https://gatsby-static-comments.netlify.com">
-                Gatsby Static Comments{" "}
-                <span className="status">(In Progress)</span>
-              </a>
-            </h3>
-            <div>
+            </span>
+            <a
+              href="https://github.com/mdarrik/sharktank-collaboration-presentation"
+              className="self-center underline text-blue-600"
+            >
+              See Code
+            </a>
+          </p>
+        </li>
+        <li className="mb-4 shadow-lg border-t-4 border-teal-400 px-4">
+          <h3 className="text-lg mb-2">
+            <a href="https://gatsby-static-comments.netlify.com">
+              Gatsby Static Comments{" "}
+              <span className="text-sm flex justify-center md:inline text-orange-700">
+                (In Progress)
+              </span>
+            </a>
+          </h3>
+          <p className="flex flex-col">
+            <span>
               Simple <abbr title="Proof Of Concept">POC</abbr> of a way to
               handle comments in <a href="https://www.gatsbyjs.org/">Gatsby</a>{" "}
-              without depending on 3rd party storage/management solutions. Uses{" "}
+              without depending on 3rd party, non-static solutions. Uses{" "}
               <a href="https://www.netlify.com/">Netlify</a> Forms &amp;
               Functions to handle comment submission, low-level spam filtering,
-              sanitization, and saving.
-              <a
-                href="https://github.com/mdarrik/gatsby-static-comments"
-                className="code-link"
-              >
-                See Code
-              </a>
-            </div>
-          </li>
-        </ul>
-      </section>
-    </div>
+              input sanitization, and saving.
+            </span>
+            <a
+              href="https://github.com/mdarrik/gatsby-static-comments"
+              className="self-center underline text-blue-600"
+            >
+              See Code
+            </a>
+          </p>
+        </li>
+      </ul>
+    </section>
   </Layout>
 )
 
