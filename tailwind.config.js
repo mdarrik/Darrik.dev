a585d68be9f1b87fbb09e56d53291300defaa29b
Darrk Moberg <30670444+mdarrik@users.noreply.github.com>
2019-07-20 21:13:29 -0700

    feature: replace default tailwind fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,23 @@
 module.exports = {
   theme: {
+    fontFamily: {
+      sans: [
+        '"Fira Sans"',
+        "-apple-system",
+        "BlinkMacSystemFont",
+        '"Segoe UI"',
+        "Roboto",
+        '"Helvetica Neue"',
+        "Arial",
+        '"Noto Sans"',
+        "sans-serif",
+        '"Apple Color Emoji"',
+        '"Segoe UI Emoji"',
+        '"Segoe UI Symbol"',
+        '"Noto Color Emoji"',
+      ],
+      decorative: ['"Fredericka the Great"', "cursive"],
+    },
     extend: {},
   },
   variants: {},
