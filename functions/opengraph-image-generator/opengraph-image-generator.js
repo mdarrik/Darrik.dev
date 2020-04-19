98404e1b7eadf7accf0a954218335ef1308b74f9
Darrik Moberg <30670444+mdarrik@users.noreply.github.com>
2020-04-18 22:18:47 -0700

    debug: comments out a bunch to debug

--- functions/opengraph-image-generator/opengraph-image-generator.js
+++ functions/opengraph-image-generator/opengraph-image-generator.js
@@ -25,61 +25,61 @@ exports.handler = async function({UserId, UserAction, queryStringParameters}, fn
         statusCode: 501,
         body: 'There was an error generating the open graph image'
     }
-    try {
+//     try {
     
-    browser = await playwright.launchChromium();
-    const context = await browser._defaultContext;
-    const page = await context.newPage();
-    page.setViewportSize({
-        width: 1200,
-        height: 630
-    })
-    await page.setContent(
-        `<!DOCTYPE html>
-        <html>
-            <head>
-                <meta charset="utf-8" />
-            </head>
-            <body>
-                <div id='container'></div>
-            </body>
-        </html>
-    `);
-    const tags = queryStringParameters.tags ? decodeURIComponent(queryStringParameters.tags).split(",") : [];
-    await page.addScriptTag({
-        content: `
-            window.title = "${queryStringParameters.title || "No Title"}"
-            window.tags = ${JSON.stringify(tags)};
-            window.author = "${queryStringParameters.author || ''}";
-        `
-    })
-    await page.addScriptTag({
-        content: script
-    })
-    const boundingRect = await page.evaluate(() => {
-        const container = document.getElementById('root-div')
-        const {x, y, width, height } = container.getBoundingClientRect();
-        return {x, y, width, height};
-    });
-    const screenshotBuffer = await page.screenshot({ clip: boundingRect})
-    returnValue = {
-        isBase64Encoded: true,
-        statusCode: 200,
-        headers: {
-            "Content-Type": "image/png",
-            "Content-Length": screenshotBuffer.length.toString()
-        },
-        body: screenshotBuffer.toString()
-    }
-    honeycombEvent.add({'screenshot-successful': true})
-}catch(err) {
-    honeycombEvent.add({'error-message': err.message, 'error-stack': err.stack, 'screenshot-successful': false});
-    returnValue.body = err.message
-}finally {
-    if(browser && browser.isConnected()) {
-        await browser.close()
-    }
-}
+//     browser = await playwright.launchChromium();
+//     const context = await browser._defaultContext;
+//     const page = await context.newPage();
+//     page.setViewportSize({
+//         width: 1200,
+//         height: 630
+//     })
+//     await page.setContent(
+//         `<!DOCTYPE html>
+//         <html>
+//             <head>
+//                 <meta charset="utf-8" />
+//             </head>
+//             <body>
+//                 <div id='container'></div>
+//             </body>
+//         </html>
+//     `);
+//     const tags = queryStringParameters.tags ? decodeURIComponent(queryStringParameters.tags).split(",") : [];
+//     await page.addScriptTag({
+//         content: `
+//             window.title = "${queryStringParameters.title || "No Title"}"
+//             window.tags = ${JSON.stringify(tags)};
+//             window.author = "${queryStringParameters.author || ''}";
+//         `
+//     })
+//     await page.addScriptTag({
+//         content: script
+//     })
+//     const boundingRect = await page.evaluate(() => {
+//         const container = document.getElementById('root-div')
+//         const {x, y, width, height } = container.getBoundingClientRect();
+//         return {x, y, width, height};
+//     });
+//     const screenshotBuffer = await page.screenshot({ clip: boundingRect})
+//     returnValue = {
+//         isBase64Encoded: true,
+//         statusCode: 200,
+//         headers: {
+//             "Content-Type": "image/png",
+//             "Content-Length": screenshotBuffer.length.toString()
+//         },
+//         body: screenshotBuffer.toString()
+//     }
+//     honeycombEvent.add({'screenshot-successful': true})
+// }catch(err) {
+//     honeycombEvent.add({'error-message': err.message, 'error-stack': err.stack, 'screenshot-successful': false});
+//     returnValue.body = err.message
+// }finally {
+//     if(browser && browser.isConnected()) {
+//         await browser.close()
+//     }
+// }
     honeycombEvent.add(returnValue)
     honeycombEvent.send()
     return returnValue
